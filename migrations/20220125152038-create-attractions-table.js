d23d9b7613dd5353e529cb59ccf42cca170a9636
diyanarmln <diyana.rmln@gmail.com>
2022-01-26 23:30:07 +0800

    same as last

---
+++ migrations/20220125152038-create-attractions-table.js
@@ -0,0 +1,34 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('attractions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      trip_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'trips',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('attractions');
+  },
+};
