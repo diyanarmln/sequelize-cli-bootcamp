c3658b5241dacacccfd5b7609a6bb7445d2da219
diyanarmln <diyana.rmln@gmail.com>
2022-01-26 23:14:06 +0800

    created db and models

---
+++ models/attraction.mjs
@@ -0,0 +1,31 @@
+export default function initAttractionModel(sequelize, DataTypes) {
+  return sequelize.define('attraction', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    trip_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'trips',
+        key: 'id',
+      },
+    },
+    created_at: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updated_at: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    // The underscored option makes Sequelize reference snake_case names in the DB.
+    underscored: true,
+  });
+}
--- models/index.mjs
+++ models/index.mjs
@@ -1,17 +1,40 @@
-import { Sequelize } from 'sequelize';
+import sequelizePackage from 'sequelize';
 import allConfig from '../config/config.js';
 
-// import model functions here
+import initTripModel from './trip.mjs';
+import initAttractionModel from './attraction.mjs';
 
+const { Sequelize } = sequelizePackage;
 const env = process.env.NODE_ENV || 'development';
 const config = allConfig[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config,
+);
 
-// add your model definitions to db here
+db.Item = initTripModel(sequelize, Sequelize.DataTypes);
+db.Attraction = initAttractionModel(sequelize, Sequelize.DataTypes);
+
+// The following 2 lines enable Sequelize to recognise the 1-M relationship
+// between Item and Category models, providing the mixin association methods.
+db.Attraction.belongsTo(db.Trip);
+db.Trip.hasMany(db.Attraction);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 export default db;
+
+db.Trip
+  .create({
+    name: process.argv[2],
+  })
+  .then((trip) => {
+    console.log('success');
+    console.log(trip);
+  })
+  .catch((error) => console.log(error));
